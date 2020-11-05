98fa046cbb9083676d30d120dce4ef29839a316e
Dhanush <dhanuraj7258@gmail.com>
2020-11-05 12:46:14 +0530

    stripe menu

---
+++ src/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    // gives center of button
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
+  };
+
+  return (
+    <nav className="nav" onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="Logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {sublinks.map((link, index) => {
+            const { page } = link;
+            return (
+              <li key={index}>
+                <button className="link-btn" onMouseOver={displaySubmenu}>
+                  {page}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="btn signin-btn">Sign in</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
